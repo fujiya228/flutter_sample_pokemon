e573cb4a1cb5f8fa15a085ded3b151b4656daa97
fujiya228 <fujiya0xE2@gmail.com>
2022-04-21 23:08:05 +0900

    ListView で 複数表示

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,25 @@ class TopPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: PokeButton()),
+    return Scaffold(
+      body: ListView(
+        children: const [
+          PokeButton(),
+          PokeButton(),
+          PokeButton(),
+          PokeButton(),
+          PokeButton(),
+          PokeButton(),
+          PokeButton(),
+          PokeButton(),
+          PokeButton(),
+          PokeButton(),
+          PokeButton(),
+          PokeButton(),
+          PokeButton(),
+          PokeButton(),
+        ],
+      ),
     );
   }
 }
