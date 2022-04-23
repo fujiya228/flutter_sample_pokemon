efbb7948bb18d58fa537e73a80b0fd8dc3cf22aa
fujiya228 <fujiya0xE2@gmail.com>
2022-04-24 00:57:39 +0900

    Convert to StaetefulWidgetを実行
    Settingsで

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import './theme_mode_selection_page.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
 
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return ListView(
