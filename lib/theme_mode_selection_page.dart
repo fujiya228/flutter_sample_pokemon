7fe44e3212419c091371ac13ab6feaa911d886c2
fujiya228 <fujiya0xE2@gmail.com>
2022-04-23 23:37:53 +0900

    ThemeModeSelectionPageウィジェットを作成

---
+++ lib/theme_mode_selection_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ThemeModeSelectionPage extends StatelessWidget {
+  const ThemeModeSelectionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            ListTile(
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () => {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
