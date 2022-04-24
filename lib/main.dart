83217b94ed4fa35ceebb225dbb5cc70439a9db39
fujiya228 <fujiya0xE2@gmail.com>
2022-04-24 22:25:15 +0900

    TopPageを分割

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import './poke_list.dart';
-import './settings.dart';
+import './top_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,44 +17,6 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData.dark(),
       themeMode: mode,
       home: const TopPage(),
-    );
-  }
-}
-
-class TopPage extends StatefulWidget {
-  const TopPage({Key? key}) : super(key: key);
-
-  @override
-  State<TopPage> createState() => _TopPageState();
-}
-
-class _TopPageState extends State<TopPage> {
-  int currentIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: currentIndex == 0 ? const PokeList() : const Settings(),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (index) => {
-          setState(
-            () => currentIndex = index,
-          )
-        },
-        currentIndex: currentIndex,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'settings',
-          ),
-        ],
-      ),
     );
   }
 }
