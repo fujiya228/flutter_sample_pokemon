547bbb475a3cf6ebb75dd412953b8d58fec66fa8
fujiya228 <fujiya0xE2@gmail.com>
2022-04-29 15:14:20 +0900

    Convert to StaetefulWidgetを実行
     PokeListで

--- lib/poke_list.dart
+++ lib/poke_list.dart
@@ -3,11 +3,16 @@ import 'package:provider/provider.dart';
 import './poke_list_item.dart';
 import 'models/pokemons_notifier.dart';
 
-class PokeList extends StatelessWidget {
+class PokeList extends StatefulWidget {
   const PokeList({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<PokeList> createState() => _PokeListState();
+}
+
+class _PokeListState extends State<PokeList> {
   @override
   Widget build(BuildContext context) {
     return Consumer<PokemonsNotifier>(
