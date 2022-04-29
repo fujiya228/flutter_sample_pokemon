6fcd10b5a6762dc7f3998394d14ed60e2fb6bf2f
fujiya228 <fujiya0xE2@gmail.com>
2022-04-29 15:23:39 +0900

    moreボタンを押すと追加で読み込むようにした

--- lib/poke_list.dart
+++ lib/poke_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './poke_list_item.dart';
+import 'consts/pokeapi.dart';
 import 'models/pokemons_notifier.dart';
 
 class PokeList extends StatefulWidget {
@@ -13,14 +14,35 @@ class PokeList extends StatefulWidget {
 }
 
 class _PokeListState extends State<PokeList> {
+  static const int more = 30;
+  int pokeCount = more;
+
   @override
   Widget build(BuildContext context) {
     return Consumer<PokemonsNotifier>(
       builder: (context, pokemons, child) => ListView.builder(
         padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-        itemCount: 10,
+        itemCount: pokeCount + 1,
         itemBuilder: (context, index) {
-          return PokeListItem(pokemon: pokemons.byId(index + 1));
+          if (index == pokeCount) {
+            return OutlinedButton(
+              child: const Text('more'),
+              onPressed: () => {
+                setState(
+                  () {
+                    pokeCount = pokeCount + more;
+                    if (pokeCount > pokeMaxId) {
+                      pokeCount = pokeMaxId;
+                    }
+                  },
+                )
+              },
+            );
+          } else {
+            return PokeListItem(
+              pokemon: pokemons.byId(index + 1),
+            );
+          }
         },
       ),
     );
