f9fffe3f95110be4c65d6e5fa3a8afd130c5e082
fujiya228 <fujiya0xE2@gmail.com>
2022-04-30 15:15:40 +0900

    SafeAreaで囲った

--- lib/poke_detail.dart
+++ lib/poke_detail.dart
@@ -12,71 +12,74 @@ class PokeDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ListTile(
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () => Navigator.pop(context),
-              ),
-            ),
-            const Spacer(),
-            Stack(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(32),
-                  child: Image.network(
-                    pokemon.imageUrl,
-                    height: 100,
-                    width: 100,
-                  ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ListTile(
+                leading: IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () => Navigator.pop(context),
                 ),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  child: Text(
-                    'No.${pokemon.id}',
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+              ),
+              const Spacer(),
+              Stack(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(32),
+                    child: Image.network(
+                      pokemon.imageUrl,
+                      height: 100,
+                      width: 100,
                     ),
                   ),
-                ),
-              ],
-            ),
-            Text(
-              pokemon.name,
-              style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: pokemon.types
-                  .map(
-                    (type) => Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8),
-                      child: Chip(
-                        backgroundColor: pokeTypeColors[type] ?? Colors.grey,
-                        label: Text(
-                          type,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: (pokeTypeColors[type] ?? Colors.grey)
-                                        .computeLuminance() >
-                                    0.5
-                                ? Colors.black
-                                : Colors.white,
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                      'No.${pokemon.id}',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                pokemon.name,
+                style:
+                    const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: pokemon.types
+                    .map(
+                      (type) => Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                        child: Chip(
+                          backgroundColor: pokeTypeColors[type] ?? Colors.grey,
+                          label: Text(
+                            type,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: (pokeTypeColors[type] ?? Colors.grey)
+                                          .computeLuminance() >
+                                      0.5
+                                  ? Colors.black
+                                  : Colors.white,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  )
-                  .toList(),
-            ),
-            const Spacer(
-              flex: 3,
-            ),
-          ],
+                    )
+                    .toList(),
+              ),
+              const Spacer(
+                flex: 3,
+              ),
+            ],
+          ),
         ),
       ),
     );
