58f6cf08b73f555c32bfc167fdd9d9ca2deb94ea
fujiya228 <fujiya0xE2@gmail.com>
2022-05-03 22:40:33 +0900

    FavoritePokeListにヘルプボタンを追加

--- lib/favorite_poke_list.dart
+++ lib/favorite_poke_list.dart
@@ -10,23 +10,88 @@ class FavoritePokeList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<PokemonsNotifier>(
-      builder: (context, pokemons, child) => ListView.builder(
-        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-        itemCount: pokemons.favoritePokeListItemCount,
-        itemBuilder: (context, index) {
-          if (index == pokemons.favoritePokeCount) {
-            return OutlinedButton(
-              child: const Text('more'),
-              onPressed: () => pokemons.updateFavoritePokeList(),
-            );
-          } else {
-            return PokeListItem(
-              pokemon: pokemons.favoritePokeByIndex(index),
-            );
-          }
-        },
-      ),
+    return Column(
+      children: [
+        Container(
+          height: 24,
+          alignment: Alignment.topRight,
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            icon: const Icon(Icons.help_outline_outlined),
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+                builder: (BuildContext context) {
+                  return Container(
+                    height: 300,
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Center(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            height: 5,
+                            width: 30,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Theme.of(context).backgroundColor,
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 24),
+                            child: Text(
+                              'お気に入りのポケモンが表示されています',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(40)),
+                            ),
+                            child: const Text('閉じる'),
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ),
+        Expanded(
+          child: Consumer<PokemonsNotifier>(
+            builder: (context, pokemons, child) => ListView.builder(
+              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+              itemCount: pokemons.favoritePokeListItemCount,
+              itemBuilder: (context, index) {
+                if (index == pokemons.favoritePokeCount) {
+                  return OutlinedButton(
+                    child: const Text('more'),
+                    onPressed: () => pokemons.updateFavoritePokeList(),
+                  );
+                } else {
+                  return PokeListItem(
+                    pokemon: pokemons.favoritePokeByIndex(index),
+                  );
+                }
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
